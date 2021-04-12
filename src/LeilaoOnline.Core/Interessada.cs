6f3804a90246450ebf8c7ca635627395a8733e44
Yuri Siman <yurisimannogueira@gmail.com>
2021-04-12 13:09:54 -0300

    Criando o projeto

---
+++ src/LeilaoOnline.Core/Interessada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeilaoOnline.Core
+{
+    public class Interessada
+    {
+        public string Nome { get; set; }
+        public Leilao Leilao { get; set; }
+
+        public Interessada(string nome, Leilao leilao)
+        {
+            Nome = nome;
+            Leilao = leilao;
+        }
+    }
+}
