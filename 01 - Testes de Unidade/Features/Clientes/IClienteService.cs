2bbe75bf7c7d0a75fb7a73d2c48f2b63e89974c9
Yuri Siman <yurisimannogueira@gmail.com>
2022-03-22 12:55:36 -0300

    Utilizando Traits nos testes de unidade

---
+++ 01 - Testes de Unidade/Features/Clientes/IClienteService.cs
@@ -0,0 +1,11 @@
+﻿namespace Features.Clientes
+{
+    public interface IClienteService : IDisposable
+    {
+        IEnumerable<Cliente> ObterTodosAtivos();
+        void Adicionar(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Remover(Cliente cliente);
+        void Inativar(Cliente cliente);
+    }
+}
---
+++ 01 - Testes de Unidade/Features/Core/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Features.Core
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        void Adicionar(TEntity entity);
+        TEntity ObterPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity entity);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+        int Commit();
+    }
+}
