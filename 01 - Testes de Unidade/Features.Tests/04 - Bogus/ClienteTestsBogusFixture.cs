6f99396ffae4bf634a1d52522fc0758cb3ac23a1
Yuri Siman <yurisimannogueira@gmail.com>
2022-04-01 14:30:30 -0300

    Simulando retorno de métodos com Mock

--- 01 - Testes de Unidade/Features.Tests/04 - Bogus/ClienteTestsBogusFixture.cs
+++ 01 - Testes de Unidade/Features.Tests/04 - Bogus/ClienteTestsBogusFixture.cs
@@ -2,6 +2,8 @@
 using Bogus.DataSets;
 using Features.Clientes;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Features.Tests
 {
@@ -9,21 +11,41 @@ namespace Features.Tests
     {
         public Cliente GerarClienteValido()
         {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        //public Cliente GerarClienteInvalido()
+        //{
+        //    return GerarClientes(1, false).FirstOrDefault();
+        //}
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
+        {
             var genero = new Faker().PickRandom<Name.Gender>();
 
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente(
                     Guid.NewGuid(),
                     f.Name.FirstName(genero),
                     f.Name.LastName(genero),
                     f.Date.Past(80, DateTime.Now.AddYears(-18)),
                     "",
-                    true,
+                    ativo,
                     DateTime.Now))
                 .RuleFor(c => c.Email, (f,c) => 
                     f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
 
-            return cliente;
+            return clientes.Generate(quantidade);
         }
 
         public Cliente GerarClienteInvalido()
--- 01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
+++ 01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Features.Clientes;
 using MediatR;
 using Moq;
+using System.Linq;
 using System.Threading;
 using Xunit;
 
@@ -40,12 +41,40 @@ namespace Features.Tests
         public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
         {
             // Arrange
-
+            var cliente = _clienteTestsBogusFixture.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
 
             // Act
-
+            clienteService.Adicionar(cliente);
 
             // Assert
+            Assert.False(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente), Times.Never);
+            mediator.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange 
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+
+            clienteRepo.Setup(c => c.ObterTodos())
+                .Returns(_clienteTestsBogusFixture.ObterClientesVariados());
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+            // Assert
+            clienteRepo.Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
         }
     }
 }
