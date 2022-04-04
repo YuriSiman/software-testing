cf37b1a4da92d1d6490f1717e6dc2f764630e764
Yuri Siman <yurisimannogueira@gmail.com>
2022-04-03 21:36:31 -0300

    Code Coverage

---
+++ 01 - Testes de Unidade/Features.Tests/09 - Code Coverage/CpfValidationTests.cs
@@ -0,0 +1,46 @@
+﻿using Features.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class CpfValidationTests
+    {
+        [Theory(DisplayName = "CPF Válidos")]
+        [Trait("Categoria", "CPF Validation Tests")]
+        [InlineData("15231766607")]
+        [InlineData("78246847333")]
+        [InlineData("64184957307")]
+        [InlineData("21681764423")]
+        [InlineData("13830803800")]
+        public void Cpf_ValidarMultiplosNumeros_TodosDevemSerValidos(string cpf)
+        {
+            // Arrange
+            var cpfValidation = new CpfValidation();
+
+            // Act & Assert
+            //Assert.True(cpfValidation.EhValido(cpf));
+
+            // Act & Assert - Fluent Assertion
+            cpfValidation.EhValido(cpf).Should().BeTrue();
+        }
+
+        [Theory(DisplayName = "CPF Inválidos")]
+        [Trait("Categoria", "CPF Validation Tests")]
+        [InlineData("15231766607123")]
+        [InlineData("12345678945")]
+        [InlineData("12358549874")]
+        [InlineData("00025401870")]
+        public void Cpf_ValidarMultiplosNumeros_TodosDevemSerInvalidos(string cpf)
+        {
+            // Arrange
+            var cpfValidation = new CpfValidation();
+
+            // Act & Assert
+            //Assert.False(cpfValidation.EhValido(cpf));
+
+            // Act & Assert - Fluent Assertion
+            cpfValidation.EhValido(cpf).Should().BeFalse();
+        }
+    }
+}
